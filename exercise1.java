989391b53b7f0f3906a00c5571c82b58175a449c
kalexakis3 <147396957+kalexakis3@users.noreply.github.com>
2023-11-06 11:20:28 +0200

    Add files via upload

---
+++ exercise1.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+//Importing the scanner class
+
+public class exercise1 {
+	
+	public static void main(String[] args){
+	//Prompting the user to intput their card number	
+	System.out.println("Enter your card number");
+	//creating the scanner class
+	Scanner scan = new Scanner(System.in);	
+	Int suit = scan.nextInt();
+	System.out.println("Now enter the suit");
+	//Making the if statement
+	scan.nextLine();
+	String name = scan.nextLine();
+
+	{if(name = c)
+		{System.out.println("Your card is Clubs" + suit);}
+
+	else if(name = s)
+		{System.out.println("Your card is Spades" + suit);}
+
+	else if(name = d)
+		{System.out.println("Your card is Diamonds" + suit);}
+
+	else (name = h);
+		{System.out.println("Your card is Hearts" + suit);}
+
+	
+		}
+	
+
+	}
+
+
+
+}
